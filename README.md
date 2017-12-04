fa0c51ffb633c0d31085411ad02ac5d87fb65edb
Krister Riska <31847374+kristerriska@users.noreply.github.com>
2017-12-04 15:26:04 +0200

    Vist parandatud failid

--- README.md
+++ README.md
@@ -1,17 +1,2 @@
-# Projekt Piltide üleslaadimise kommuun
-# 2: suurelt projekti veebirakenduse pilt
-
-Krister Riska, Norman Salong  
-
-Üritame luua veebilehte, kus saab üles laadide pilte. Avalikke pilte saab lehitseda veebilehe külastaja. Kasutajad saavad üksteist lisada  "sõbraks", et näha ainult sõpradele nähtavaid pilte. On ka privaatsed pildid, mis on näha ainult kasutajale endale.
-
-Sihtrühmaks on fotograafiaga või fototöötlusega tegelevad inimesed. Eripäraks võib välja tuua, et veebilehte võib kasutada ka ainult oma  piltide hoiustamiseks, ning juurde on antud ka avalik jagamine ja privaatne sõprade vahel jagamine.
-
-1: Saab registreerida kasutajaks ja sisselogida;  
-2: Saab pilte üles laadida või neid kustutada;  
-3: Saab pilte muuta avalikuks, ainult sõpradele nähtavaks või privaatseks;  
-4: Saab enda profiili täiustada;  
-5: Saab lisada sõpru või vajadusel neid eemaldada;  
-6: Saab kommenteerida sõprade või avalikke pilte;  
-
-
+# veebiprogremiseprojekt
+Veebiprogremmeerimise rühmatöö 2017
--- login/main.php
+++ login/main.php
@@ -37,7 +37,7 @@
 	</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="css/w3.css">
 		<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-blue-grey.css">
 		<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -51,8 +51,9 @@
 <div class="w3-top">
  <div class="w3-bar w3-theme-d2 w3-left-align w3-large">
   <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
-  <a href="#" class="w3-bar-item w3-button w3-padding-large w3-theme-d4"><i class="fa fa-home w3-margin-right"></i>Kodu</a>
+  <a href="http://greeny.cs.tlu.ee/~riskkris/veebiprogremiseprojekt/login/main.php" class="w3-bar-item w3-button w3-padding-large w3-theme-d4"><i class="fa fa-home w3-margin-right"></i>Kodu</a>
   <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" title="Account Settings"><i class="fa fa-user"></i></a>
+  <a href="?logout=1">Logi välja</a>
   <div class="w3-dropdown-hover w3-hide-small">
     <button class="w3-button w3-padding-large" title="Notifications"><i class="fa fa-bell"></i><span class="w3-badge w3-right w3-small w3-green">3</span></button>     
     <div class="w3-dropdown-content w3-card-4 w3-bar-block" style="width:300px">
@@ -61,7 +62,7 @@
       <a href="#" class="w3-bar-item w3-button">Jane likes your post</a>
     </div>
   </div>
-  <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" title="My Account"><img src="/w3images/avatar2.png" class="w3-circle" style="height:25px;width:25px" alt="Avatar"></a>
+  <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" title="My Account"><img src="./pics/banaan.png" class="w3-circle" style="height:25px;width:25px" alt="Avatar"></a>
  </div>
 </div>
 
@@ -69,32 +70,70 @@
 <div id="navDemo" class="w3-bar-block w3-theme-d2 w3-hide w3-hide-large w3-hide-medium w3-large">
   <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 1</a>
   <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 2</a>
-  <p><a href="?logout=1">Logi välja</a></p>
+  <a href="?logout=1">Logi välja</a>
   <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 3</a>
   <a href="#" class="w3-bar-item w3-button w3-padding-large">My Profile</a>
 </div>
 
 <!-- Page Container -->
-<div class="w3-container w3-content" style="max-width:1400px;margin-top:80px">    
-  <!-- The Grid -->
-  <div class="w3-row">
-    <!-- Left Column -->
+<div class="w3-container w3-content" style="max-width:1600px;margin-top:50px">    
+  <!-- Algus -->
+  <div class="w3-row">   
+    
+    <!-- Põhikolumn -->
+    <div class="w3-col m7" style="margin-top:10px">
+    
+      <div class="w3-row-padding">
+        <div class="w3-col m12">
+          <div class="w3-card w3-round w3-white">
+            <div class="w3-container w3-padding">
+              <h6 class="w3-opacity">Tänane päev on hea piltide lisamiseks.</h6>
+              <p contenteditable="true" class="w3-border w3-padding"></p>
+              <button type="button" class="w3-button w3-theme"><i class="fa fa-pencil"></i>  Vali fail</button>
+			  <button type="button" class="w3-button w3-theme"><i class="fa fa-pencil"></i>  Postita</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="w3-container w3-card w3-white w3-round w3-margin"><br>
+        <img src="./pics/banaan.png" alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px">
+        <span class="w3-right w3-opacity">1 min</span>
+        <h4><?php echo $_SESSION["firstname"] ." " .$_SESSION["lastname"]; ?></h4><br>
+        <hr class="w3-clear">
+        <p>Mingid lahedad pildid, et liiga tühi ei tunduks.</p>
+          <div class="w3-row-padding" style="margin:0 -16px">
+            <div class="w3-half">
+              <img src="./pics/river.jpg" style="width:100%" alt="Northern Lights" class="w3-margin-bottom">
+            </div>
+            <div class="w3-half">
+              <img src="./pics/mets.jpg" style="width:100%" alt="Nature" class="w3-margin-bottom">
+          </div>
+        </div>
+        <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"><i class="fa fa-thumbs-up"></i>  Meeldib</button> 
+        <button type="button" class="w3-button w3-theme-d2 w3-margin-bottom"><i class="fa fa-comment"></i>  Kommenteeri</button> 
+      </div> 
+      
+    <!-- Lõpp Põhikolumn -->
+    </div>
+	
+	<!-- Vasak kolumn -->
     <div class="w3-col m3">
-      <!-- Profile -->
-      <div class="w3-card w3-round w3-white">
+      <!-- Profiil -->
+      <div class="w3-card w3-round w3-white w3-center">
         <div class="w3-container">
          <h4 class="w3-center"><?php echo $_SESSION["firstname"] ." " .$_SESSION["lastname"]; ?></h4>
          <p class="w3-center"><img src="./pics/banaan.png" class="w3-circle" style="height:106px;width:106px" alt="banaan"></p>
          <hr>
-         <p><i class="fa fa-birthday-cake fa-fw w3-margin-right w3-text-theme"></i> April 1, 1988</p>
+         <p><i class="fa fa-birthday-cake fa-fw w3-margin-right w3-text-theme"></i> 16. veebruar, 1998</p>
         </div>
       </div>
       <br>
       
-      <!-- Accordion -->
+      <!-- Piltide dropdown -->
       <div class="w3-card w3-round">
         <div class="w3-white">
-          <button onclick="myFunction('Demo3')" class="w3-button w3-block w3-theme-l1 w3-left-align"><i class="fa fa-users fa-fw w3-margin-right"></i> My Photos</button>
+          <button onclick="myFunction('Demo3')" class="w3-button w3-block w3-theme-l1 w3-left-align"><i class="fa fa-users fa-fw w3-margin-right"></i> Minu Pildid</button>
           <div id="Demo3" class="w3-hide w3-container">
          <div class="w3-row-padding">
          <br>
@@ -121,48 +160,9 @@
         </div>      
       </div>
       <br>
-
-    <!-- End Left Column -->
+	<!-- Lõpp vasak kolumn -->
     </div>
-    
-    <!-- Middle Column -->
-    <div class="w3-col m7">
-    
-      <div class="w3-row-padding">
-        <div class="w3-col m12">
-          <div class="w3-card w3-round w3-white">
-            <div class="w3-container w3-padding">
-              <h6 class="w3-opacity">Tänane päev on hea kunsti lisamiseks.</h6>
-              <p contenteditable="true" class="w3-border w3-padding"></p>
-              <button type="button" class="w3-button w3-theme"><i class="fa fa-pencil"></i>  Vali fail</button>
-			  <button type="button" class="w3-button w3-theme"><i class="fa fa-pencil"></i>  Postita</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-      <div class="w3-container w3-card w3-white w3-round w3-margin"><br>
-        <img src="/w3images/avatar2.png" alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px">
-        <span class="w3-right w3-opacity">1 min</span>
-        <h4>John Doe</h4><br>
-        <hr class="w3-clear">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <div class="w3-row-padding" style="margin:0 -16px">
-            <div class="w3-half">
-              <img src="/w3images/lights.jpg" style="width:100%" alt="Northern Lights" class="w3-margin-bottom">
-            </div>
-            <div class="w3-half">
-              <img src="/w3images/nature.jpg" style="width:100%" alt="Nature" class="w3-margin-bottom">
-          </div>
-        </div>
-        <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"><i class="fa fa-thumbs-up"></i>  Like</button> 
-        <button type="button" class="w3-button w3-theme-d2 w3-margin-bottom"><i class="fa fa-comment"></i>  Comment</button> 
-      </div> 
-      
-    <!-- End Middle Column -->
-    </div>
-    
-  <!-- End Grid -->
+  <!-- Lõpp -->
   </div>
   
 <!-- End Page Container -->
@@ -170,8 +170,8 @@
 <br>
 
 <!-- Footer -->
-<footer class="w3-container w3-theme-d3 w3-padding-16">
-  <h5>Footer</h5>
+<footer class="w3-container w3-theme-d5">
+  <p>Veebiprogremise projekt</p>
 </footer>
  
 <script>
